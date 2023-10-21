94a77ed2ca7803cb49c724b9f54cb2f6efe3fa3a
NICOL1201 <117902091+NICOL120@users.noreply.github.com>
2023-10-22 01:39:33 +0800

    Update PrimarySlice.tsx

--- apps/enterprise/src/pages/landing/PrimarySlice.tsx
+++ apps/enterprise/src/pages/landing/PrimarySlice.tsx
@@ -2,29 +2,23 @@ import classNames from 'classnames';
 import { Text } from 'lib/ui/Text';
 import { useDisplay } from 'hooks';
 import { useNavigate } from 'react-router';
-import styles from './PrimarySlice.module.sass';
-import { Button } from 'lib/ui/buttons/Button';
+import styles from './PrimarySlice.module.css';
 import { HStack } from 'lib/ui/Stack';
 import { ExternalLink } from 'components/link';
 
 export const PrimarySlice = () => {
   const navigate = useNavigate();
-
   const { isMobile } = useDisplay();
 
   return (
-    <div className={classNames(styles.root, { [styles.mobile]: isMobile })}>
+    <div className={classNames(styles.btn, { [styles.mobile]: isMobile })}>
       <Text size={40} weight="bold">
         Nico Money Dao
       </Text>
       <HStack alignItems="center" gap={20}>
-        <Button
-          onClick={() => {
-            navigate('dashboard');
-          }}
-        >
+        <button className={classNames(styles.btn)} onClick={() => navigate('dashboard')}>
           Launch App
-        </Button>
+        </button>
       </HStack>
     </div>
   );
