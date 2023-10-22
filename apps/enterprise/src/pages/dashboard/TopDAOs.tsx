6151621eced11d12013ac9454edf939e490cdd2c
NICOL1201 <117902091+NICOL120@users.noreply.github.com>
2023-10-22 23:54:34 +0800

    Update TopDAOs.tsx

--- apps/enterprise/src/pages/dashboard/TopDAOs.tsx
+++ apps/enterprise/src/pages/dashboard/TopDAOs.tsx
@@ -11,8 +11,7 @@ export const RecentDAOs = () => {
   const daos = data.sort((a, b) => (b.tvl ?? 0) - (a.tvl ?? 0)).slice(0, 6);
 
 return (
-    <div className={classNames(styles.card)}>
-      <h2>
+
       <LabeledPageSection name="">
       <SameWidthChildrenRow maxColumns={3} fullWidth minChildrenWidth={320} gap={16}>
         {daos.map((dao, index) => (
@@ -20,7 +19,6 @@ return (
         ))}
       </SameWidthChildrenRow>
     </LabeledPageSection>
-      </h2>
-      </div>
+    
   );
 };
